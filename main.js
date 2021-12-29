ed87f4c1f366916e8ce98b17a5d66988d017db84
BineetKarmakar <83464472+BineetKarmakar@users.noreply.github.com>
2021-12-29 20:38:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 //https://teachablemachine.withgoogle.com/models/8nrvs6LNo/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/8nrvs6LNo/model.json', modelReady());
+}
+
+function modelReady() {
+    classifier.classify(gotResult)
+};
+function gotResult(error,results){
+    console.log("gotResult")
+}
